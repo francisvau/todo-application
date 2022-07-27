32201c1a2904a612d4bee21e67050faa3cc08a34
francisvau <vauterinf@gmail.com>
2022-07-27 18:39:10 +0200

    todolist.js

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+
+import TodoList from "./todolist";
+
+const theme = document.querySelector(".header__theme");
+const newCard = document.querySelector(".dashboard__new-card");
+const tasks = document.querySelector(".dashboard__tasks");
+
+const amountLeft = document.querySelector(".tasks-info__amount-left");
+const clearFinished = document.querySelector(".tasks-info__clear-finished");
+
+const filterAll = document.querySelector(".tasks-filter__all");
+const filterActive = document.querySelector(".tasks-filter__active");
+const filterCompleted = document.querySelector(".tasks-filter__completed");
---
+++ scripts/todolist.js
@@ -0,0 +1,17 @@
+export class TodoList {
+    constructor() {
+        this.tasks = [];
+    }
+
+    addTask(task) {
+        this.tasks.push(task);
+    }
+
+    removeTask(task) {
+        this.tasks.remove(task);
+    }
+
+    switchFinished(task) {
+        task["finished"] = !task["finished"];
+    }
+}
